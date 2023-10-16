7a2504c0b219b2b63e8d0944c435483a3f3720e0
unknown <gujjarlapudiketansai@gmail.com>
2023-10-16 17:25:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Calculator from './components/C1'
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
